600817de8c82c8b3c0d6c22c7c64258e13309633
a.mahrous <a.mahrousmahmoud@gmail.com>
2021-11-13 19:27:00 +0200

    Submitted version, all working 13-11-2021

--- server.js
+++ server.js
@@ -2,18 +2,41 @@
 projectData = {};
 
 // Require Express to run server and routes
+const express = require('express');
 
 // Start up an instance of app
+const app = express();
 
 /* Middleware*/
+const bodyParser = require('body-parser');
+
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
-
 // Setup Server
+const port = 8080;
+const server = app.listen(port, () => {
+    console.log("server is running");
+    console.log(`running on: localhost:${port}`);
+});
+
+
+app.get("/getAll", (req, res) => {
+    //console.log(projectData);
+    return res.send(projectData);
+});
+
+app.post("/add", (req, res) => {
+    const receivedData = req.body;
+    projectData = receivedData;
+    //console.log(projectData);
+    return res.send(JSON.stringify(projectData));
+});
--- website/app.js
+++ website/app.js
@@ -1,5 +1,115 @@
 /* Global Variables */
 
+//const { fetch } = require(['cross-fetch']);
+let city_name = "Cairo";
+const API_key = "50cb8d8f87c1d0e56c798e3ceefca76f";
+let base_url = `https://api.openweathermap.org/data/2.5/weather?q=`;
+
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+// Event listener to add function to existing HTML DOM element
+var stateCode = "";
+var user_feelings = "";
+document.getElementById('zip').addEventListener('input',(event) => {
+        stateCode = event.target.value;
+});
+
+document.getElementById('feelings').addEventListener('input',(event) => {
+        user_feelings = event.target.value;
+    });
+
+document.getElementById('generate').addEventListener('click', doSomething);
+
+async function doSomething () {
+
+    const data = await getWebAPIData(base_url, stateCode, API_key)
+    .then(async (data) => {
+        const PostedProjectData = await postData("http://localhost:8080/add",
+                {
+                    "temperature": data.main.temp.toString(),
+                    "date": newDate,
+                    "user_response": user_feelings
+                }
+        )
+        /*.then((PostedProjectData) => {
+            updateUI(PostedProjectData.temperature, PostedProjectData.date, PostedProjectData.user_response);
+        });*/
+        .then(async ()=>{
+            const projectData = await getProjectData("http://localhost:8080/getAll","");
+            updateUI(projectData.temperature, projectData.date, projectData.user_response);
+        });
+    });
+};
+
+/* Function to POST data */
+const postData = async (url, data) => {
+    const response = await fetch(url,
+    {
+        method: 'POST',
+        credentials: 'same-origin',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(data)
+    });
+    try
+    {
+        const newPost = await response.json();
+        //console.log(newPost);
+        return newPost;
+    }
+    catch (error)
+    {
+        console.log(error);
+    }
+};
+
+/* Function to GET Web API Data*/
+const getWebAPIData = async (base_url, stateCode, API_key) => 
+{
+    const full_url = `${base_url}${city_name},${stateCode}&appid=${API_key}`;
+    console.log(full_url);
+    const response = await fetch(full_url);
+    try 
+    {
+        const data = await response.json();
+        //console.log(data);
+        return data;
+    }
+    catch(error)
+    {
+        console.log(error);
+    }
+
+};
+
+/* Function to GET Project Data */
+const getProjectData = async (base_url) => 
+{
+    const response = await fetch(base_url);
+    try 
+    {
+        const data = await response.json();
+        //console.log(data);
+        return data;
+    }
+    catch(error)
+    {
+        console.log(error);
+    }
+
+};
+
+/* Function to update UI */ 
+const updateUI = (temp, date, feelings) => {
+    try
+    {
+        document.getElementById('temp').innerHTML = `Temperature: ${temp}.`;
+        document.getElementById('date').innerHTML = `Date: ${date}.`;
+        document.getElementById('content').innerHTML = `User feelings: ${feelings}.`;
+    }
+    catch(error)
+    {
+        console.log(error);
+    }
+};
